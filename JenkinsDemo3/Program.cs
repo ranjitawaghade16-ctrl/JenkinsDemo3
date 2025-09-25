74d8075da0f6b837b705c69590e92a30f0852c39
ranji <ranjitawaghade16@gmail.com>
2025-09-25 17:15:10 +0530

    Add project files.

---
+++ JenkinsDemo3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JenkinsDemo3
+{
+    internal class Program
+    {
+        public int Add(int a, int b)
+        {
+           int c = a + b;
+            return c;
+        }
+
+        public int Multi(int a, int b)
+        {
+            int c = a * b;
+            return c;
+        }
+
+        public int Div(int a, int b)
+        {
+            int c = a / b;
+            return c;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Add(10, 20);
+            program.Multi(10, 30);
+            program.Div(10, 40);
+            
+        }
+    }
+}
